e799cffb2a9cb1f8ea4157ededd877fd145dc402
arvind0251 <160855021+arvind0251@users.noreply.github.com>
2025-05-25 16:19:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-API_ID = 123456        # Your Telegram API ID (from https://my.telegram.org)
-API_HASH = "your_api_hash_here"
-BOT_TOKEN = "your_bot_token_here"
-MONGO_URI = "mongodb://localhost:27017/"  # or your MongoDB Atlas URI
-LOG_CHANNEL = -1001234567890              # Optional: logs admin actions here
-OWNER_ID = 123456789                      # Your Telegram user ID
+API_ID = 21552265       # Your Telegram API ID (from https://my.telegram.org)
+API_HASH = "1c971ae7e62cc416ca977e040e700d09"
+BOT_TOKEN = "7508328129:AAF4ZGj0tf69Y1S3_oAdWJ-w86Q2Gqe7pC0"
+MONGO_URI = "mongodb+srv://KARTIK:KARTIK.23@kartik.flltt.mongodb.net/?retryWrites=true&w=majority&appName=KARTIK"  # or your MongoDB Atlas URI
+LOG_CHANNEL = -1002136294449             # Optional: logs admin actions here
+OWNER_ID = 7408008545                      # Your Telegram user ID
